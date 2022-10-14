7db431ba8884249f3c5e8193ed5256ef6989c22d
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2022-10-14 20:04:18 +0800

    style(encryption): change code style in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 fn main() {
     let mut str = String::new();
-    println!("input chars");
+    println!("input mode,1 is encryption,0 is decryption");
     std::io::stdin()
         .read_line(&mut str)
         .expect("Error at read value");
+    if str == "1\r\n".to_string() {
+        std::io::stdin()
+            .read_line(&mut str)
+            .expect("Error at read value two");
+        encryption(str);
+    }
+}
+fn encryption(str: String) {
     let mut binary_num: Vec<usize> = Vec::new();
-    for c in str[0..str.len() - 2].chars() {
-        let mut a: i32 = c as i32;
+    for e in str[3..str.len() - 2].chars() {
+        let mut a: i32 = e as i32;
         let mut c = 0;
 
         while c != 1 {
