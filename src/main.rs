f18d5266054be08fdf9c180daf877b58debf4794
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2022-10-14 21:12:24 +0800

    feat(decryption): added decryption func in main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ fn main() {
             .read_line(&mut str)
             .expect("Error at read value two");
         encryption(str);
+    }else if str == "0\r\n".to_string() {
+        std::io::stdin()
+            .read_line(&mut str)
+            .expect("Error at read value two");
+        decryption(str);
     }
 }
 fn encryption(str: String) {
@@ -16,10 +21,11 @@ fn encryption(str: String) {
     for e in str[3..str.len() - 2].chars() {
         let mut a: i32 = e as i32;
         let mut c = 0;
-
+        print!("{a}");
         while c != 1 {
             let b = (a % 2) as usize;
             binary_num.push(b);
+            
             a /= 2;
             c = a;
         }
@@ -33,4 +39,24 @@ fn encryption(str: String) {
         }
         println!("");
     }
+}
+fn decryption(str: String) {
+    let mut i=str.len()-5;
+    let mut sum=0;
+    for c in str[3..str.len()-2].chars() {
+        i-=1;
+        if c=='*'{
+            let mut t=i;
+            let mut sum1=1;
+            while t>0{
+                sum1=sum1*2;
+                t-=1;
+            }
+            sum+=sum1;
+        }
+        
+    }
+    let c: char=char::from_u32(sum).unwrap();
+    println!("{c}");
+    
 }
